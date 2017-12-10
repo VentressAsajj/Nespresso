925371b0ee88be54ea088a076172e80e0d28bc51
Asajj Ventress <hermanadelanoche@gmail.com>
2017-12-10 23:54:13 +0100

    Add files via upload

--- nespresso.ino
+++ nespresso.ino
@@ -5,6 +5,11 @@
  * @nuria_pp
  * https://github.com/VentressAsajj/Nespresso
  */
+/*
+ * 
+ */
+
+ 
 /* Librerias */
 
 /* LCD */
@@ -17,13 +22,13 @@
 #include <Wire.h>
 
 /* Definicion de pines */
+/* pin2 LCD */
 #define SERLCD_PIN_RX  2
 
 /* Definicion de variables globales y objetos*/
-uint16_t rgbSensorR,rgbSensorG,rgbSensorB;
 
 /* Declaracion objento de sensor RGB */
-SFE_ISL29125 rgbSensor;
+SFE_ISL29125 RGB_Sensor;
 
 /* Declaracion del objeto pantalla 16x2 LCD */
 SoftwareSerial serlcdNSS(0,SERLCD_PIN_RX);
@@ -36,41 +41,60 @@ const long interval = 4000;
 bool detect = 0;
 
 
-/* This code sets up the essentials for your circuit to work. It runs firsGt every time your circuit is powered with electricity. */
+/* Configure */
 void setup() {
-  // Setup Serial which is useful for debugging
-  // Use the Serial Monitor to view printed messages
+
+  /* Inicializacion del monitor serie para control de errores y demas */
   Serial.begin(115200);
-  Serial.println("start");
-  pinMode(8, OUTPUT);
+  Serial.println("Iniciando programa");
+  pinMode(8, OUTPUT); 
   pinMode(9, OUTPUT);
 
   serlcdNSS.begin(9600); 
   serlcd.begin();   
-  serlcd.setPosition(1,2);
-  serlcd.print("Hola Nuria"); 
-  serlcd.setPosition(2,2);
-  serlcd.print("dia duro,huh?");  
-  delay(5000);
-      
-  // Initialize the rgbSensor
-  bool rgbSensorInitStatus = rgbSensor.init();
-  if (rgbSensorInitStatus)
-    Serial.println("rgbSensor Init OK!");
+  serlcd.setPosition(1,0);
+  serlcd.print("Buenos dias nena"); 
+  serlcd.setPosition(2,0);
+  serlcd.print("estas estupenda");  
+  delay(4000);
+  /* Detectando sensor RGB */    
+  if (RGB_Sensor.init())
+    Serial.println("Inicializacion correcta del sensor RBG");
   else
-    Serial.println("rgbSensor Init Failed! Check your wiring.");
+    Serial.println("Error iniciando sensor RBG");
+    serlcd.setPosition(1,0);
+    serlcd.print("Error sensor RBG"); 
+    delay(4000);
 }
 
-/* This code is the main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop. */
+/* principal */
 void loop() {
-  unsigned long currentMillis = millis();
+  /* Devuelve el numero de milisegundos desde que el arduino se ha reseteado.
+   * Ojo cuando llega a los 50 dias se resetea.
+  */
+  unsigned long currentMillis = millis(); 
+  Serial.println("Milisegundos: ");
+  Serial.println(currentMillis);
 
-  if (rgbSensor.readBlue() < 1) {
+  /* leemos valores del sensor, 16bits integers*/
+  unsigned int red = RGB_Sensor.readRed();
+  unsigned int green = RGB_Sensor.readGreen();
+  unsigned int blue = RGB_Sensor.readBlue();
+  Serial.print("Red: "); Serial.println(red,HEX);
+  Serial.print("Green: "); Serial.println(green,HEX);
+  Serial.print("Blue: "); Serial.println(blue,HEX);
+  Serial.println();
+  delay(4000);
+
+  
+  if (RGB_Sensor.readBlue() < 1) {
     ledState = HIGH;
     digitalWrite(8, HIGH);
     digitalWrite(9, HIGH);
     detect = 1;
   }
+  Serial.println("Detectado: ");
+  Serial.print(detect);
   if (currentMillis - previousMillis >= interval && ledState == HIGH) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
@@ -80,110 +104,88 @@ void loop() {
     serlcd.setBrightness(1);
   }
 
-  int red = rgbSensor.readRed();
-  int green = rgbSensor.readGreen();
-  int blue = rgbSensor.readBlue();
-
+  /* Valores RGB para cada cafe */
+  /*
   int cosi[3] = {229, 389, 329};
-  //int dulsao[3] = {142, 253, 191};
-  int dulsao[3] = {6, 253, 191};
+  int dulsao[3] = {142, 253, 191};
   int roma[3] = {83, 196, 177};
   int livanto[3] = {188, 299, 187};
   int vivalto_lungo [3] = {134, 294, 449};
   int bukeela_lungo[3] = {330, 452, 319};
-  //int kazaar[3] = {60, 165, 180};
-  int kazaar[3] = {5, 10, 5};
+  int kazaar[3] = {60, 165, 180};
   int volluto[3] = {263, 421, 248};
+  */
 
-  int thresh = 20 ;
+  int cosi[3] = {229, 389, 329};
+  int roma[3] = {83, 196, 177};
+  int livanto[3] = {188, 299, 187};
+  int vivalto_lungo [3] = {134, 294, 449};
+  int volluto[3] = {263, 421, 248};
   
+  int kazaar[3] = {60, 165, 180};
+  int dulsao[3] = {142, 253, 191};
+  int bukeela_lungo[3] = {330, 452, 319};
+  
+  int thresh = 20 ;
+  /* comentar cuando ya se tenga los valores RGB para cada cafe*/
   Serial.print("Red:");
-  Serial.print(rgbSensor.readRed());
+  Serial.print(RGB_Sensor.readRed());
   Serial.print("    Green:");
-  Serial.print(rgbSensor.readGreen());
+  Serial.print(RGB_Sensor.readGreen());
   Serial.print("    Blue:");
-  Serial.print(rgbSensor.readBlue());
+  Serial.print(RGB_Sensor.readBlue());
   Serial.println();
   delay(3000);
   
+  detect = 1;
   if (detect) {
 
-    //Cosi
+    /* Cosi */
     if (abs(red - cosi[0]) < thresh && abs(green - cosi[1]) < thresh && abs(blue - cosi[2]) < thresh ) {
       Serial.println("Cosi");
       serlcd.setBrightness(28);
       serlcd.clear();
-      serlcd.setPosition(1,2);
+      serlcd.setPosition(1,0);
       serlcd.print("Cosi");
-      serlcd.setPosition(2,3);
-      serlcd.print("Intensity: 4/10");
-      delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 40ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Fruity");
-      delay(2500);
-      serlcd.clear();
+      serlcd.setPosition(2,0);
+      serlcd.print("Intensidad: 4");
+      delay(4000);
     }
 
-
-    //Dulsao
+    /* Dulsao */
     if (abs(red - dulsao[0]) < thresh && abs(green - dulsao[1]) < thresh && abs(blue - dulsao[2]) < thresh ) {
       Serial.println("Dulsao");
       serlcd.setBrightness(28);
       serlcd.clear();
-      serlcd.setPosition(1,2);
+      serlcd.setPosition(1,0);
       serlcd.print("Dulsao");
-      serlcd.setPosition(2,3);
-      serlcd.print("Intensity: 4/10");
-      delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 40ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Sweet Cereal");
-      delay(2500);
-      serlcd.clear();
+      serlcd.setPosition(2,0);
+      serlcd.print("Intensidad: 4");
+      delay(4000);
     }
 
-    //Roma
+    /* Roma */
     if (abs(red - roma[0]) < thresh && abs(green - roma[1]) < thresh && abs(blue - roma[2]) < thresh ) {
       Serial.println("Roma");
       serlcd.setBrightness(28);
       serlcd.clear();
-      serlcd.setPosition(1,2);
+      serlcd.setPosition(1,0);
       serlcd.print("Roma");
-      serlcd.setPosition(2,3);
-      serlcd.print("Intensity: 8/10");
-      delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 25ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Woody");
-      delay(2500);
-      serlcd.clear();
+      serlcd.setPosition(2,0);
+      serlcd.print("Intensidad: 8");
+      delay(4000);
     }
 
-    //Livanto
-
+    /* Livanto */
     if (abs(red - livanto[0]) < thresh && abs(green - livanto[1]) < thresh && abs(blue - livanto[2]) < thresh ) {
       Serial.println("Livanto");
       serlcd.setBrightness(28);
       serlcd.clear();
-      serlcd.setPosition(1,2);
+      serlcd.setPosition(1,0);
       serlcd.print("Livanto");
-      serlcd.setPosition(2,3);
-      serlcd.print("Intensity: 6/10");
-      delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 40ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Caramel");
-      delay(2500);
-      serlcd.clear();
+      serlcd.setPosition(2,0);
+      serlcd.print("Intensidad: 6");
+      delay(4000);
     }
 
     //Vivalto Lungo
@@ -196,14 +198,7 @@ void loop() {
       serlcd.setPosition(2,3);
       serlcd.print("Intensity: 4/10");
       delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 110ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Floral,Roasted");
-      delay(2500);
-      serlcd.clear();
-    }
+     }
 
     //Bukeela Lungo
     if (abs(red - bukeela_lungo[0]) < thresh && abs(green - bukeela_lungo[1]) < thresh && abs(blue - bukeela_lungo[2]) < thresh ) {
@@ -215,49 +210,36 @@ void loop() {
       serlcd.setPosition(2,3);
       serlcd.print("Intensity: 3/10");
       delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 110ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Wild, Floral");
-      delay(2500);
-      serlcd.clear(); 
     }
 
-    //Kazaar
+    /* Kazaar */
     if (abs(red - kazaar[0]) < thresh && abs(green - kazaar[1]) < thresh && abs(blue - kazaar[2]) < thresh ) {
       Serial.print("Kazaar");
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Kazaar");
       serlcd.setBrightness(28);
+      serlcd.clear();
+      serlcd.setPosition(1,0);
+      serlcd.print("Kazaar");
+      serlcd.setPosition(2,0);
+      serlcd.print("Intensidad: 10");
       delay(4000);
       serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Intensidad 12"); /* mañana con sueño */
-      serlcd.setPosition(2,3);
-      serlcd.print("mucho sueño");  
+      serlcd.setPosition(1,0);
+      serlcd.print("Presiento que hoy");
+      serlcd.setPosition(2,0);
+      serlcd.print("es un dia duro");
       delay(4000);
-      serlcd.clear();
     }
 
-    //Volluto
+    /* Volluto */
     if (abs(red - volluto[0]) < thresh && abs(green - volluto[1]) < thresh && abs(blue - volluto[2]) < thresh ) {
       Serial.println("Volluto");
       serlcd.setBrightness(28);
       serlcd.clear();
-      serlcd.setPosition(1,2);
+      serlcd.setPosition(1,0);
       serlcd.print("Volluto");
-      serlcd.setPosition(2,3);
-      serlcd.print("Intensity: 4/10");
-      delay(2500);
-      serlcd.clear();
-      serlcd.setPosition(1,2);
-      serlcd.print("Cup Size: 40ml");
-      serlcd.setPosition(2,3);
-      serlcd.print("Cereal Fruity");
-      delay(2500);
-      serlcd.clear();
+      serlcd.setPosition(2,0);
+      serlcd.print("Intensidad: 4");
+      delay(4000);
     }
   }
 }
